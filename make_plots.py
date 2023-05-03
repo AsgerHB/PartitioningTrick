302268d05f46b50ed40d7ddf00aa2a875971eca8
Peter G. Jensen <root@petergjoel.dk>
2023-05-03 11:43:58 +0200

    cleaning up

--- make_plots.py
+++ make_plots.py
@@ -1,3 +1,4 @@
+#/bin/python3
 import os
 import pandas
 import random
@@ -14,19 +15,19 @@ def make_plots(input_file, output_dir):
     df = pandas.read_csv(input_file, sep="\s*;\s*", engine='python') # Dumbass csv-parser doesn't know to trim whitespace by itself!
     df = preprocess(df)
     
-    for AP in [-1, 0, 1, 2, 3]:
+    """for AP in [-1, 0, 1, 2, 3]:
         for AV in [-1, 0, 1, 2, 3]:
             plot_learning_rates(df, AP, AV)
             plt.savefig(os.path.join(learning_rates_dir, f"AP{AP}_AV{AV}.png"))
-            plt.close()
+            plt.close()"""
     for R in [100, 1000, 5000, 10000, 25000]:
         plot_agents(df, R)
         plt.savefig(os.path.join(agents_dir, f"R{R}.png"))
         plt.close()
 
 def preprocess(df):
-    df["gamma"] = 15/df["GP"]
-    df["gamma2"] = 30/df["GV"]
+    df["gamma"] = df["GP"]/15
+    df["gamma2"] = df["GV"]/(2*15)
     df["sanity_check"] = df["gamma"] == df["gamma2"]
     if not all(df["sanity_check"]):
         raise Exception("Mismatch between GV and GP. It was not the case that 15/GP == 30/GV. (unknown row)")
@@ -47,19 +48,32 @@ def plot_learning_rates(df, AP, AV):
 
 def plot_agents(df, R):
     fig, ax = plt.subplots()
+
+    ax.set_yscale('log')
     fig.set_figheight(8)
     fig.set_figwidth(12)
 
     df = df.sort_values(by=['gamma'])
 
-    df1 = df[(df["R"] == R) & (df["AP"] == -1) & (df["AV"] == -1)]
-    ax = df1.plot(ax=ax, x="gamma", y="reward", ylabel="reward", label=f"AP=-1, AV=-1")
+    
+    df1 = df[(df["R"] == R)]
+    df1 = df1[["gamma", "reward"]].groupby('gamma').min()
+    print(df1) 
+    ax = df1.plot(ax=ax, y="reward", ylabel="reward", label=f"MIN")
 
-    df1 = df[(df["R"] == R) & (df["AP"] == 1) & (df["AV"] == 1)]
-    ax = df1.plot(ax=ax, x="gamma", y="reward", ylabel="reward", label=f"AP=1, AV=1")
+    df1 = df[(df["R"] == R)]
+    df1 = df1[["gamma", "reward"]].groupby('gamma').max()
+    ax = df1.plot(ax=ax, y="reward", ylabel="reward", label=f"MAX")
 
     df1 = df[(df["R"] == R) & (df["AP"] == 2) & (df["AV"] == 2)]
-    ax = df1.plot(ax=ax, x="gamma", y="reward", ylabel="reward", label=f"AP=2, AV=2")
+    ax = df1.plot(ax=ax, x="gamma", y="reward", ylabel="reward", label=f"Uniform")
+
+    df1 = df[(df["R"] == R) & (df["AP"] == -1) & (df["AV"] == -1)]
+    ax = df1.plot(ax=ax, x="gamma", y="reward", ylabel="reward", label=f"Lower,Lower")
+
+    df1 = df[(df["R"] == R) & (df["AP"] == 1) & (df["AV"] == 1)]
+    ax = df1.plot(ax=ax, x="gamma", y="reward", ylabel="reward", label=f"UpperUpper")
+    ax.set_xlabel("1/delta")
 
 if __name__ == "__main__":
     import argparse
