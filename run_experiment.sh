8d448c8c8845f3a9c90aa73d645ea40ac55a6d96
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-17 10:33:47 +0200

    Remove unused runs

--- run_experiment.sh
+++ run_experiment.sh
@@ -59,17 +59,31 @@ for RUNS in 5000 10000 ; do #5000 10000 25000 50000 100000 250000 500000 ; do #1
         export AGENT_R=2
         $EXECUTOR ./run_single.sh
 
-        for AGENT_I in -1 1 ; do
-            for AGENT_V in -1 1 ; do
-                for AGENT_R in -1 1 ; do
-                    # same SAMPLE_COST as AGENT_I
-                    export SAMPLED_COST=$AGENT_I
-                    export AGENT_I=$AGENT_I
-                    export AGENT_V=$AGENT_V
-                    export AGENT_R=$AGENT_R
-                    $EXECUTOR ./run_single.sh
-                done
-            done
-        done
+        # Lower+worst cost
+        export SAMPLED_COST=-1
+        export AGENT_I=-1
+        export AGENT_V=-1
+        export AGENT_R=-1
+        $EXECUTOR ./run_single.sh
+
+        # Upper+best cost
+        export SAMPLED_COST=1
+        export AGENT_I=1
+        export AGENT_V=1
+        export AGENT_R=1
+        $EXECUTOR ./run_single.sh
+
+        # for AGENT_I in -1 1 ; do
+        #     for AGENT_V in -1 1 ; do
+        #         for AGENT_R in -1 1 ; do
+        #             # same SAMPLE_COST as AGENT_I
+        #             export SAMPLED_COST=$AGENT_I
+        #             export AGENT_I=$AGENT_I
+        #             export AGENT_V=$AGENT_V
+        #             export AGENT_R=$AGENT_R
+        #             $EXECUTOR ./run_single.sh
+        #         done
+        #     done
+        # done
     done
 done
