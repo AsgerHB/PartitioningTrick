e5cfda13810cc4bb4487cdef8187146cf3cb9035
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-18 15:45:26 +0200

    Actually let's try running DC now

--- run_BB.sh
+++ run_BB.sh
@@ -6,60 +6,60 @@ export UPPAAL=uppaal-rqvar/
 for RUNS in 10000 ; do # 5000 10000 25000 50000 100000 250000 500000 ; do
     export RUNS=$RUNS
     for NGRID in $(seq 15 30 1500) ; do 
-    export GRID_P=$NGRID
-    export GRID_V=$(($NGRID*2))
+        export GRID_P=$NGRID
+        export GRID_V=$(($NGRID*2))
 
-    # Uniform + sampled cost
-    export SAMPLED_COST=2
-    export AGENT_V=2
-    export AGENT_P=2
-    $EXECUTOR ./run_single.sh
+        # Uniform + sampled cost
+        export SAMPLED_COST=2
+        export AGENT_V=2
+        export AGENT_P=2
+        $EXECUTOR ./run_single.sh
 
-    # Historical + sampled cost
-    export SAMPLED_COST=2
-    export AGENT_V=3
-    export AGENT_P=3
-    $EXECUTOR ./run_single.sh
+        # Historical + sampled cost
+        export SAMPLED_COST=2
+        export AGENT_V=3
+        export AGENT_P=3
+        $EXECUTOR ./run_single.sh
 
-    # Best + sampled cost
-    # export SAMPLED_COST=2
-    # export AGENT_V=-3
-    # export AGENT_P=1
-    # $EXECUTOR ./run_single.sh
+        # Best + sampled cost
+        # export SAMPLED_COST=2
+        # export AGENT_V=-3
+        # export AGENT_P=1
+        # $EXECUTOR ./run_single.sh
 
-    # Worst + sampled cost
-    # export SAMPLED_COST=2
-    # export AGENT_V=-2
-    # export AGENT_P=-1
-    # $EXECUTOR ./run_single.sh
+        # Worst + sampled cost
+        # export SAMPLED_COST=2
+        # export AGENT_V=-2
+        # export AGENT_P=-1
+        # $EXECUTOR ./run_single.sh
 
-    # Best + best cost
-    export SAMPLED_COST=1
-    export AGENT_V=-3
-    export AGENT_P=1
-    $EXECUTOR ./run_single.sh
+        # Best + best cost
+        export SAMPLED_COST=1
+        export AGENT_V=-3
+        export AGENT_P=1
+        $EXECUTOR ./run_single.sh
 
-    # Worst + worst cost
-    export SAMPLED_COST=-1
-    export AGENT_V=-2
-    export AGENT_P=-1
-    $EXECUTOR ./run_single.sh
+        # Worst + worst cost
+        export SAMPLED_COST=-1
+        export AGENT_V=-2
+        export AGENT_P=-1
+        $EXECUTOR ./run_single.sh
 
 
-    # Uniform + static cost
-    export SAMPLED_COST=0
-    export AGENT_V=2
-    export AGENT_P=2
-    $EXECUTOR ./run_single.sh
+        # Uniform + static cost
+        export SAMPLED_COST=0
+        export AGENT_V=2
+        export AGENT_P=2
+        $EXECUTOR ./run_single.sh
 
-    # for AGENT_P in -1 1 ; do
-    #         for AGENT_V in -1 1 2 ; do
-    # 	# same SAMPLE_COST as AGENT_P
-    # 	export SAMPLED_COST=$AGENT_P
-    #             export AGENT_V=$AGENT_V
-    #             export AGENT_P=$AGENT_P
-    #             $EXECUTOR ./run_single.sh
-    #         done
-    #     done
+        # for AGENT_P in -1 1 ; do
+        #         for AGENT_V in -1 1 2 ; do
+        # 	# same SAMPLE_COST as AGENT_P
+        # 	export SAMPLED_COST=$AGENT_P
+        #             export AGENT_V=$AGENT_V
+        #             export AGENT_P=$AGENT_P
+        #             $EXECUTOR ./run_single.sh
+        #         done
+        #     done
     done
 done
--- run_DC.sh
+++ run_DC.sh
@@ -5,7 +5,7 @@ export UPPAAL=uppaal-rqvar/
 
 for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
     export RUNS=$RUNS
-    for NGRID in $(seq 15 60 1500) ; do 
+    for NGRID in $(seq 15 30 1500) ; do 
         export GRID_I=$NGRID
         export GRID_V=$NGRID
         export GRID_R=$NGRID
@@ -17,12 +17,12 @@ for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
         export AGENT_R=2
         $EXECUTOR ./run_single.sh
 
-        # # Historical + sampled cost
-        # export SAMPLED_COST=2
-        # export AGENT_I=3
-        # export AGENT_V=3
-        # export AGENT_R=3
-        # $EXECUTOR ./run_single.sh
+        # Historical + sampled cost
+        export SAMPLED_COST=2
+        export AGENT_I=3
+        export AGENT_V=3
+        export AGENT_R=3
+        $EXECUTOR ./run_single.sh
 
         # # Best + sampled cost
         # export SAMPLED_COST=2
@@ -38,12 +38,12 @@ for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
         # export AGENT_R=-2
         # $EXECUTOR ./run_single.sh
 
-        # # Best + best cost
-        # export SAMPLED_COST=1
-        # export AGENT_I=-3
-        # export AGENT_V=-3
-        # export AGENT_R=-3
-        # $EXECUTOR ./run_single.sh
+        # Best + best cost
+        export SAMPLED_COST=1
+        export AGENT_I=-3
+        export AGENT_V=-3
+        export AGENT_R=-3
+        $EXECUTOR ./run_single.sh
 
         # Worst + worst cost
         export SAMPLED_COST=-1
@@ -52,37 +52,37 @@ for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
         export AGENT_R=-2
         $EXECUTOR ./run_single.sh
 
-        # # Uniform + static cost
-        # export SAMPLED_COST=0
-        # export AGENT_I=2
-        # export AGENT_V=2
-        # export AGENT_R=2
-        # $EXECUTOR ./run_single.sh
+        # Uniform + static cost
+        export SAMPLED_COST=0
+        export AGENT_I=2
+        export AGENT_V=2
+        export AGENT_R=2
+        $EXECUTOR ./run_single.sh
 
-        # # Lower+worst cost
-        # export SAMPLED_COST=-1
-        # export AGENT_I=-1
-        # export AGENT_V=-1
-        # export AGENT_R=-1
-        # $EXECUTOR ./run_single.sh
+        # Lower+worst cost
+        export SAMPLED_COST=-1
+        export AGENT_I=-1
+        export AGENT_V=-1
+        export AGENT_R=-1
+        $EXECUTOR ./run_single.sh
 
-        # # Upper+best cost
-        # export SAMPLED_COST=1
-        # export AGENT_I=1
-        # export AGENT_V=1
-        # export AGENT_R=1
-        # $EXECUTOR ./run_single.sh
+        # Upper+best cost
+        export SAMPLED_COST=1
+        export AGENT_I=1
+        export AGENT_V=1
+        export AGENT_R=1
+        $EXECUTOR ./run_single.sh
 
-        for AGENT_I in -1 1 ; do
-            for AGENT_V in -1 1 ; do
-                for AGENT_R in -1 1 ; do
-                    export SAMPLED_COST=0
-                    export AGENT_I=$AGENT_I
-                    export AGENT_V=$AGENT_V
-                    export AGENT_R=$AGENT_R
-                    $EXECUTOR ./run_single.sh
-                done
-            done
-        done
+        # for AGENT_I in -1 1 ; do
+        #     for AGENT_V in -1 1 ; do
+        #         for AGENT_R in -1 1 ; do
+        #             export SAMPLED_COST=0
+        #             export AGENT_I=$AGENT_I
+        #             export AGENT_V=$AGENT_V
+        #             export AGENT_R=$AGENT_R
+        #             $EXECUTOR ./run_single.sh
+        #         done
+        #     done
+        # done
     done
 done
