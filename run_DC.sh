9c435eefca2e145665e8adb9e34bb580a3d5223d
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-18 10:34:14 +0200

    I also wanna see these DC agents

--- run_DC.sh
+++ run_DC.sh
@@ -10,19 +10,19 @@ for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
         export GRID_V=$NGRID
         export GRID_R=$NGRID
 
-        # # Uniform + sampled cost
-        # export SAMPLED_COST=2
-        # export AGENT_I=2
-        # export AGENT_V=2
-        # export AGENT_R=2
-        # $EXECUTOR ./run_single.sh
+        # Uniform + sampled cost
+        export SAMPLED_COST=2
+        export AGENT_I=2
+        export AGENT_V=2
+        export AGENT_R=2
+        $EXECUTOR ./run_single.sh
 
-        # # Historical + sampled cost
-        # export SAMPLED_COST=2
-        # export AGENT_I=3
-        # export AGENT_V=3
-        # export AGENT_R=3
-        # $EXECUTOR ./run_single.sh
+        # Historical + sampled cost
+        export SAMPLED_COST=2
+        export AGENT_I=3
+        export AGENT_V=3
+        export AGENT_R=3
+        $EXECUTOR ./run_single.sh
 
         # # Best + sampled cost
         # export SAMPLED_COST=2
@@ -38,19 +38,19 @@ for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
         # export AGENT_R=-2
         # $EXECUTOR ./run_single.sh
 
-        # # Best + best cost
-        # export SAMPLED_COST=1
-        # export AGENT_I=-3
-        # export AGENT_V=-3
-        # export AGENT_R=-3
-        # $EXECUTOR ./run_single.sh
+        # Best + best cost
+        export SAMPLED_COST=1
+        export AGENT_I=-3
+        export AGENT_V=-3
+        export AGENT_R=-3
+        $EXECUTOR ./run_single.sh
 
-        # # Worst + worst cost
-        # export SAMPLED_COST=-1
-        # export AGENT_I=-2
-        # export AGENT_V=-2
-        # export AGENT_R=-2
-        # $EXECUTOR ./run_single.sh
+        # Worst + worst cost
+        export SAMPLED_COST=-1
+        export AGENT_I=-2
+        export AGENT_V=-2
+        export AGENT_R=-2
+        $EXECUTOR ./run_single.sh
 
         # # Uniform + static cost
         # export SAMPLED_COST=0
@@ -73,19 +73,19 @@ for RUNS in 10000; do #5000 10000 25000 50000 100000 250000 500000 ; do
         # export AGENT_R=1
         # $EXECUTOR ./run_single.sh
 
-        for C in -1 1; do
-            for AGENT_I in -1 1 ; do
-                for AGENT_V in -1 1 ; do
-                    for AGENT_R in -1 1 ; do
-                        # same SAMPLE_COST as AGENT_I
-                        export SAMPLED_COST=$C
-                        export AGENT_I=$AGENT_I
-                        export AGENT_V=$AGENT_V
-                        export AGENT_R=$AGENT_R
-                        $EXECUTOR ./run_single.sh
-                    done
-                done
-            done
-        done
+        # for C in -1 1; do
+        #     for AGENT_I in -1 1 ; do
+        #         for AGENT_V in -1 1 ; do
+        #             for AGENT_R in -1 1 ; do
+        #                 # same SAMPLE_COST as AGENT_I
+        #                 export SAMPLED_COST=$C
+        #                 export AGENT_I=$AGENT_I
+        #                 export AGENT_V=$AGENT_V
+        #                 export AGENT_R=$AGENT_R
+        #                 $EXECUTOR ./run_single.sh
+        #             done
+        #         done
+        #     done
+        # done
     done
 done
