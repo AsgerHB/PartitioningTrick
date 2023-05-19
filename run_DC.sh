88664d157be2e4873cb8755e7de80642fbf03627
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-19 12:12:52 +0200

    Prepare for a test-run

--- run_DC.sh
+++ run_DC.sh
@@ -3,7 +3,7 @@
 export NAME=DC
 export UPPAAL=uppaal-533581a/
 
-for RUNS in 5000 10000 25000 50000 100000 250000 500000 ; do
+for RUNS in 5000 ; do # 5000 10000 25000 50000 100000 250000 500000 ; do
     export RUNS=$RUNS
     for NGRID in $(seq 15 15 1500) ; do 
         export GRID_I=$NGRID
