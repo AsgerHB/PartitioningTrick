4e78eec84b37f826ced802bfdfb2b09cbc0b10db
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-05-19 13:28:03 +0200

    Remove learning rate parameter

--- run_single.sh
+++ run_single.sh
@@ -16,6 +16,6 @@ cd $DIR
 echo "$DIR"
 
 #R=$( /usr/bin/time -v $CUR/uppaal-5.0.0-rc2-linux64/bin/verifyta.sh -s --good-runs $RUNS --total-runs $RUNS --max-iterations 1 --eval-runs 1 -D 0.05 $CUR/models/${NAME}_partitioned.xml 2>&1)
-R=$( /usr/bin/time -v $CUR/$UPPAAL/verifyta.sh --random-search-limit 1 --deterministic-search-limit 1 --qlearning-alpha 120000000 -s --good-runs $RUNS --total-runs $RUNS --max-iterations 1 --eval-runs 1 -D 0.05 $CUR/models/${NAME}_partitioned.xml 2>&1)
+R=$( /usr/bin/time -v $CUR/$UPPAAL/verifyta.sh --random-search-limit 1 --deterministic-search-limit 1 -s --good-runs $RUNS --total-runs $RUNS --max-iterations 1 --eval-runs 1 -D 0.05 $CUR/models/${NAME}_partitioned.xml 2>&1)
 
 echo "$R" > out
